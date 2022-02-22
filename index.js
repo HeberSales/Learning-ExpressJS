e3a9f02218850de13e68e39df3a175fb36262ecf
Heber Sales <heberssales@gmail.com>
2022-02-22 17:21:49 -0300

    Entendendo get e post

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const dotenv = require('dotenv');
 const express = require('express');
+const path = require('path');
 
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || '0.0.0.0';
 
 
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
 app.get('/', (req, res) => {
-    res.send("Hello!");
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
 })
+
+app.post('/login', (req, res) => {
+    const username = req.body['username'];
+    const email = req.body['mail'];
+
+    console.log(username);
+    console.log(email);
+
+    res.send("Login!");
+})
+
+
 
 app.listen(PORT, HOST, () => {
     console.log("Example App");
