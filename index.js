7c1df840c8da8d720350f41e615c64a08fadf011
Heber Sales <heberssales@gmail.com>
2022-02-22 14:13:13 -0300

    Adicionando dotenv

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+const dotenv = require('dotenv');
 const express = require('express');
 
+dotenv.config();
+
 const app = express();
-const PORT = 3000
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || '0.0.0.0';
+
 
 app.get('/', (req, res) => {
     res.send("Hello!");
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, HOST, () => {
     console.log("Example App");
 })
+
+//depois de fazer o .env tem que por a biblioteca dele npm install dotenv
